d1387df866ec39fddf7efd4643ebef1a15cf5ecc
Sho <cozymncfc0401@gmail.com>
2025-09-28 21:58:20 +0900

    first commit

---
+++ grid.js
@@ -0,0 +1,63 @@
+const container = document.getElementById("container");
+const resetButton = document.getElementById("reset-button");
+let currentMode = "default";
+
+function getRandomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
+function createGrid(size) {
+    container.innerHTML = "";
+
+    const squareSize = 800 / size;
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.dataset.opacity = 0;
+
+        square.addEventListener("mouseover", () => {
+            if (currentMode === "default") {
+                square.style.backgroundColor = "rgb(221, 208, 252)";
+                square.style.opacity = "1";
+            } else if (currentMode === "random") {
+                square.style.backgroundColor = getRandomRGB();
+                square.style.opacity = "1";
+            } else if (currentMode === "gray") {
+                if (!square.dataset.opacity) square.dataset.opacity = 0.1;
+                else square.dataset.opacity = Math.min(parseFloat(square.dataset.opacity) + 0.1, 1);
+                square.style.backgroundColor = "rgb(75, 3, 242)";
+                square.style.opacity = square.dataset.opacity;
+            }
+        });
+        container.appendChild(square);
+    }
+}
+function createGrid_js(size) {
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+    }
+}
+
+document.getElementById("defaultMode").addEventListener("click", () => currentMode = "default");
+document.getElementById("randomMode").addEventListener("click", () => currentMode = "random");
+document.getElementById("grayMode").addEventListener("click", () => currentMode = "gray");
+
+createGrid(16);
+
+resetButton.addEventListener("click", () => {
+    let newSize = prompt("Enter new grid size (maximum 100):");
+    newSize = parseInt(newSize);
+
+    if (newSize > 0 && newSize <= 100) {
+        createGrid(newSize);
+    } else {
+        alert("Please enter a number between 1 and 100!");
+    }
+});
